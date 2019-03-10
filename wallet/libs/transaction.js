5a48ba2882cb7be4cca161e27024c74a6bf89ba5
JaSv <orcwarrior_9@hotmail.com>
2019-03-10 12:37:39 -0700

    adding transactions support

---
+++ wallet/libs/transaction.js
@@ -0,0 +1,35 @@
+const ChainUtil = require("../../chain-util");
+
+class Transaction {
+  constructor() {
+    this.id = ChainUtil.id();
+    this.input = null;
+    this.output = [];
+  }
+
+  static newTransaction(senderWallet, recipientAddress, amount) {
+    const transaction = new this();
+
+    if (amount > senderWallet.balance) {
+      console.log(`Amount: ${amount} exceeds balance: ${senderWallet.balance}`);
+      return;
+    }
+
+    transaction.output.push(
+      ...[
+        {
+          amount: senderWallet.balance - amount,
+          address: senderWallet.publicKey
+        },
+        {
+          amount,
+          address: recipient
+        }
+      ]
+    );
+
+    return transaction;
+  }
+}
+
+module.exports = Transaction;
